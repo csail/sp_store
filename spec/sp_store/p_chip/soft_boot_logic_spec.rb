18db241b22f21b23a0c83d70ce2f0d5e2bcbde4a
Victor Costan <costan@gmail.com>
2011-04-02 03:51:01 -0400

    Specs for the P chip's first boot step.

--- spec/sp_store/p_chip/soft_boot_logic_spec.rb
+++ spec/sp_store/p_chip/soft_boot_logic_spec.rb
@@ -1,4 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
 
 describe SpStore::PChip::SoftBootLogic do
+  let(:p_key) { SpStore::Crypto.sk_key }
+  let(:puf_syndrome) { SpStore::Crypto.crypto_hash p_key }
+  let(:endorsement_key) { SpStore::Crypto.key_pair }
+  let(:ca_keys) { SpStore::Mocks::FactoryKeys.ca_keys }
+  let(:endorsement_certificate) do
+    dn = { 'CN' => 'Mock P Chip' }
+    SpStore::Crypto.cert dn, 1, ca_keys, SpStore::Mocks::FactoryKeys.ca_cert,
+                         endorsement_key[:public]
+  end
+  
+  let(:observer) do
+    observer = mock('observer')
+    observer.stub!(:reset)
+    observer.stub!(:booted)
+    observer
+  end
+  let(:boot_logic) do
+    SpStore::PChip::SoftBootLogic.new p_key, ca_keys[:public], observer
+  end
+  let(:s_chip) do
+    SpStore::Mocks::SoftSChip.new p_key, endorsement_key,
+        endorsement_certificate, puf_syndrome, root_hash
+  end
+
+  before do
+    @puf_syndrome = puf_syndrome
+    @endorsement_certificate = endorsement_certificate
+    @boot_logic = boot_logic
+    @s_chip = s_chip
+  end
+  
+  it_should_behave_like 'a boot logic block'
+  
+  describe 'after boot_start' do
+    before do
+      @encrypted_nonce, @nonce_hmac =
+          boot_logic.boot_start puf_syndrome, endorsement_certificate
+      @boot_nonce = SpStore::Crypto.sk_decrypt p_key, @encrypted_nonce
+    end
+    
+    it 'resulting hmac should match the boot nonce' do
+      @nonce_hmac.should == SpStore::Crypto.hmac(p_key, @boot_nonce)
+    end
+    
+    let(:root_hash) { SpStore::Crypto.crypto_hash 'root' }
+  end
 end
--- spec/support/shared_examples/boot_logic_examples.rb
+++ spec/support/shared_examples/boot_logic_examples.rb
@@ -1,3 +1,42 @@
 shared_examples_for 'a boot logic block' do
+  describe 'reset' do
+    it 'should not raise errors' do
+      lambda {
+        @boot_logic.reset
+      }.should_not raise_error
+    end
+  end
   
+  describe 'boot_start' do
+    it 'should accept a correct syndrome and certificate' do
+      lambda {
+        @boot_logic.boot_start @puf_syndrome, @endorsement_certificate
+      }.should_not raise_error
+    end
+    
+    it 'should reject an incorrect syndrome' do
+      lambda {
+        @boot_logic.boot_start @puf_syndrome.reverse, @endorsement_certificate
+      }.should raise_error(RuntimeError)
+    end
+    
+    it 'should reject an incorrect certificate' do
+      fake_ca_keys = SpStore::Crypto.key_pair
+      fake_ca_cert = SpStore::Crypto.cert @endorsement_certificate.issuer, 3650,
+                                          fake_ca_keys
+      fake_keys = SpStore::Crypto.key_pair
+      fake_cert = SpStore::Crypto.cert @endorsement_certificate.subject, 1,
+          fake_ca_keys, fake_ca_cert, fake_keys[:public]
+      lambda {
+        @boot_logic.boot_start @puf_syndrome, fake_cert
+      }.should raise_error(RuntimeError)
+    end
+    
+    it 'should produce results accepted by the s chip' do
+      lambda {
+        @s_chip.boot(*@boot_logic.boot_start(@puf_syndrome,
+                                             @endorsement_certificate))
+      }.should_not raise_error
+    end
+  end
 end
